9e120e6b4bf3be9ef4b5e0b605cde677515fc665
rspacerr <61034617+rspacerr@users.noreply.github.com>
2026-02-28 19:09:16 -0500

    trial: svelte-kit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import './layout.css';
+  import Navbar from '$lib/components/Navbar.svelte';
+	import logo from '$lib/assets/logo.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={logo} /></svelte:head>
+
+<header>
+  <Navbar />
+</header>
+
+<main>
+  {@render children()}
+</main>
+
+<footer>
+  <!-- TODO: footer -->
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<section id="title">
+  <h1>title</h1>
+  <div id="title-splash">
+    <p>
+      I'm sure there will be a lot of useful information here
+    </p>
+  </div>
+</section>
