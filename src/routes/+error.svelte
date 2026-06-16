7a2d40e32a0f519df097faeace09b12692777df4
rspacerr <61034617+rspacerr@users.noreply.github.com>
2026-06-16 17:31:05 -0400

    add: error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import { page } from "$app/state";
+</script>
+
+<h1>{page.status} | {page.error ? page.error.message : "Something unexpected happened, please check in later!"}</h1>
